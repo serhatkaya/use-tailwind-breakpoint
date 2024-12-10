d80b44a74285a53a0546ba976c2cedb1f35ba2e2
serhatkaya <serhatkaya.sr@gmail.com>
2024-12-10 13:11:48 +0300

    fix: fix breakpoint type

--- src/create.ts
+++ src/create.ts
@@ -2,26 +2,22 @@ import { useMemo, useState } from "react";
 
 import { isBrowser, useIsomorphicEffect } from "./utils";
 
-export function create<TScreens extends BreakpointType>(screens: TScreens) {
-  function useBreakpoint(
-    breakpoint: keyof TScreens,
-    defaultValue: boolean = false
-  ) {
+export function create<TScreens extends Record<string, string | { min?: string; max?: string }>>(screens: TScreens) {
+  function useBreakpoint(breakpoint: keyof TScreens, defaultValue: boolean = false) {
     const [match, setMatch] = useState(() => defaultValue);
 
     useIsomorphicEffect(() => {
-      if (!(isBrowser && 'matchMedia' in window && window.matchMedia))
-        return undefined;
+      if (!(isBrowser && "matchMedia" in window && window.matchMedia)) return undefined;
 
       const value = screens[breakpoint];
-      let queryStr = '';
+      let queryStr = "";
 
-      if (typeof value === 'string') {
+      if (typeof value === "string") {
         queryStr = `(min-width: ${value})`;
-      } else if (typeof value === 'object') {
+      } else if (typeof value === "object") {
         const { min, max } = value;
         if (min) queryStr += `(min-width: ${min})`;
-        if (max) queryStr += `${min ? ' and ' : ''}(max-width: ${max})`;
+        if (max) queryStr += `${min ? " and " : ""}(max-width: ${max})`;
       }
 
       const query = window.matchMedia(queryStr);
@@ -32,32 +28,22 @@ export function create<TScreens extends BreakpointType>(screens: TScreens) {
 
       setMatch(query.matches);
 
-      query.addEventListener('change', listener);
-      return () => query.removeEventListener('change', listener);
+      query.addEventListener("change", listener);
+      return () => query.removeEventListener("change", listener);
     }, [breakpoint, defaultValue]);
 
     return match;
   }
 
-  function useBreakpointEffect(
-    breakpoint: keyof TScreens,
-    effect: (match: boolean) => void
-  ) {
+  function useBreakpointEffect(breakpoint: keyof TScreens, effect: (match: boolean) => void) {
     const match = useBreakpoint(breakpoint);
     useIsomorphicEffect(() => effect(match), [breakpoint, effect]);
     return null;
   }
 
-  function useBreakpointValue<T, U>(
-    breakpoint: keyof TScreens,
-    valid: T,
-    invalid: U
-  ) {
+  function useBreakpointValue<T, U>(breakpoint: keyof TScreens, valid: T, invalid: U) {
     const match = useBreakpoint(breakpoint);
-    const value = useMemo(
-      () => (match ? valid : invalid),
-      [invalid, match, valid]
-    );
+    const value = useMemo(() => (match ? valid : invalid), [invalid, match, valid]);
     return value;
   }
 
